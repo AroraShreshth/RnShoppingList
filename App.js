36d084703a9127bb726ce6a25a0bd133bfc4e35a
Shreshth Arora <sarora_be18@thapar.edu>
2020-04-21 13:23:57 +0530

    kittens integration

--- App.js
+++ App.js
@@ -1,70 +1,23 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
-import Header from './components/Header';
-import uuid from 'uuid-random';
-import ListItem from './components/ListItem';
-import AddItem from './components/AddItem';
-const App = () => {
-  const [items, setItems] = useState([
-    { id: uuid(), text: 'Milk' },
-    { id: uuid(), text: 'Bread' },
-    { id: uuid(), text: 'Juice' },
-    { id: uuid(), text: 'Paneer' },
-    { id: uuid(), text: 'Dal' },
-    { id: uuid(), text: 'Butter' },
-  ]);
+
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import App from './App Test';
 
 
-  const deleteItem = (id) => {
-    setItems(prevItems => {
-      return prevItems.filter(item => item.id != id)
-    });
-  }
+const HomeScreen = () => (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text category='h1'>Just Another App</Text>
+    </Layout>
+);
 
-  const addItem = (text) => {
-    setItems(prevItems => {
-      return [{ id: uuid(), text }, ...prevItems];
-    });
-  }
+const KittenApp = () => (
+    <ApplicationProvider {...eva} theme={eva.light}>
+        <HomeScreen />
+    </ApplicationProvider>
+)
 
-  // Math.max((i) => prevItems[i].id) + 1
-
-
-  return (
-
-    <View style={styles.container}>
-      <Header />
-      <AddItem addItem={addItem} />
-      <FlatList
-        data={items}
-        renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem} />}
-      // keyExtractor={item => item.id}
-      />
-      {/* <Text >User Application !</Text>
-      <Image style={styles.img} source={{ uri: 'https://randomuser.me/api/portraits/lego/5.jpg' }}></Image> */}
-    </View>
-  )
-}
-
-Header.defaultProps = {
-  title: 'Shopping List'
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 60
-  },
-  // texty: {
-  //   color: 'blue',
-  //   fontSize: 30
-  // },
-  // img: {
-  //   width: 100,
-  //   height: 100,
-  //   borderRadius: 50
-  // }
-
-})
-
-export default App;
+export default () => (
+    <KittenApp />
+    <App />
+);
--- components/Header.js
+++ components/Header.js
@@ -15,13 +15,23 @@ const Header = (props) => {
 const styles = StyleSheet.create({
     header: {
         height: 60,
-        padding: 15,
-        backgroundColor: 'darkslateblue',
 
+        padding: 15,
+        backgroundColor: 'green',
+        borderRadius: 15,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 9,
     },
     text: {
         color: 'white',
-        fontSize: 23,
+        fontSize: 20,
         textAlign: 'center'
     }
 });
